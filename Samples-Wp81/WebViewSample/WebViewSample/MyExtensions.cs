107581ec9ef90389698b8f0cf9416c694a163561
Nabil Safatli <nabilsafatli@hotmail.com>
2016-05-19 09:45:01 -0300

    Addition of WebView sample Here it is created a WebView with an attached property HTML You can change the source to a specific page or inject a HTML content directly to the WebView

---
+++ Samples-Wp81/WebViewSample/WebViewSample/MyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace WebViewSample
+{
+    public class MyExtensions
+    {
+        public static string GetHtml(DependencyObject obj)
+        {
+            return (string)obj.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(DependencyObject obj, string value)
+        {
+            obj.SetValue(HtmlProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for HTML.  This enables animation, styling, binding, etc… 
+                
+        public static readonly DependencyProperty HtmlProperty =
+           DependencyProperty.RegisterAttached("Html", typeof(string), typeof(MyExtensions), new PropertyMetadata("", OnHtmlChanged));
+
+        private static void OnHtmlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var wv = d as WebView;
+            wv?.NavigateToString((string)e.NewValue);
+        }
+    }
+}
---
+++ Samples-Wp81/WebViewSample/WebViewSample/SecondPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace WebViewSample
+{   
+    public sealed partial class SecondPage : Page
+    {
+        public SecondPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        public string HtmlText => "<html><body><h1>C# is AWESOME!</h1></body></html>";
+    }
+}
