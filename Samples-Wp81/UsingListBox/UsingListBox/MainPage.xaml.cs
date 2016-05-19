89efd4f2e83be588cd7a6f1e1175313793b200e8
Nabil Safatli <nabilsafatli@hotmail.com>
2016-05-19 07:39:10 -0300

    Addition of ListBox sample Here it is created a ListBox that goes over a list of a specific model and bind its values in textblocks

---
+++ Samples-Wp81/UsingListBox/UsingListBox/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace UsingListBox
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            this.DataContext = this;
+        }
+
+        public List<SystemTech> Technologies => new List<SystemTech>
+            {
+                new SystemTech
+                {
+                    Company = "Apple",
+                    Platform = "iOS",
+                    Technology = "Swift"
+                },
+                new SystemTech
+                {
+                    Company = "Microsoft",
+                    Platform = "Windows",
+                    Technology = "C#"
+                },
+                new SystemTech
+                {
+                    Company = "Google",
+                    Platform = "Android",
+                    Technology = "Java"
+                }
+            };
+    }
+}
---
+++ Samples-Wp81/UsingListBox/UsingListBox/SystemTech.cs
@@ -0,0 +1,9 @@
+﻿namespace UsingListBox
+{
+    public class SystemTech
+    {
+        public string Technology { get; set; }
+        public string Platform { get; set; }
+        public string Company { get; set; }
+    }
+}
