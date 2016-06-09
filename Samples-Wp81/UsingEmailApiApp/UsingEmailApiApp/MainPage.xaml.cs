0d987265c610afd5e87dec7bf2a5824f80d3fd36
Nabil Safatli <nabilsafatli@hotmail.com>
2016-06-09 09:26:32 -0300

    Sample that shows how to send an email with WP8.1 Creation of a EmailMessage, Recipient and Subject

---
+++ Samples-Wp81/UsingEmailApiApp/UsingEmailApiApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.ApplicationModel.Email;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace UsingEmailApiApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        private async void BtnSend_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            var emailRecipient = new EmailRecipient
+            {
+                Address = TxbRecipient.Text
+            };
+
+            var emailMessage = new EmailMessage
+            {
+                Subject = TxbSubject.Text,
+                Body = TxbBody.Text                
+            };
+            emailMessage.To.Add(emailRecipient);
+
+            await EmailManager.ShowComposeNewEmailAsync(emailMessage);
+        }
+    }
+}
