6d0d0aa94df601af45455d87135a91cacf8232bd
Nabil Safatli <nabilsafatli@hotmail.com>
2016-06-15 08:19:57 -0300

    Sample that shows how to call a phone number with WP8.1 Using PhoneCallManager.ShowPhoneCallUI

---
+++ Samples-Wp81/UsingPhoneApiApp/UsingPhoneApiApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.ApplicationModel.Calls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace UsingPhoneApiApp
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void BtnCallNumber_OnClick(object sender, RoutedEventArgs e)
+        {
+            var phoneNumber = TxbPhoneNumber.Text;
+            var displayName = TxbDisplayName.Text;
+            PhoneCallManager.ShowPhoneCallUI(phoneNumber, displayName);
+        }
+    }
+}
