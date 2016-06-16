9119c53e1593a8f7ca818b11f850757c4c09d88d
Nabil Safatli <nabilsafatli@hotmail.com>
2016-06-16 08:45:46 -0300

    Sample that shows how to share status in installed apps with WP8.1

---
+++ Samples-Wp81/UsingShareApiSample/UsingShareApiSample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace UsingShareApiSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
+        }
+
+        private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            var request = args.Request;
+            var deferral = request.GetDeferral();
+            request.Data.Properties.Title = "This is my Title";
+            request.Data.Properties.Description = "This is my Description";
+            var text = TxbUserStatus.Text;
+            request.Data.SetText(text);
+            deferral.Complete();
+        }
+
+        private void BtnShare_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
+        }
+    }
+}
