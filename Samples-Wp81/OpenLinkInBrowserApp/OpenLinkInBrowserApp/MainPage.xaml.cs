7a34e0d9012f2b6c007da3dd30aa5fc866b694b9
Nabil Safatli <nabilsafatli@hotmail.com>
2016-06-16 07:43:05 -0300

    [Update] Open Web Browser Sample with WP8.1

--- Samples-Wp81/OpenLinkInBrowserApp/OpenLinkInBrowserApp/MainPage.xaml.cs
+++ Samples-Wp81/OpenLinkInBrowserApp/OpenLinkInBrowserApp/MainPage.xaml.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
 namespace OpenLinkInBrowserApp
-{   
+{
     public sealed partial class MainPage : Page
     {
         public MainPage()
@@ -48,6 +38,5 @@ namespace OpenLinkInBrowserApp
             var uri = new Uri(url);
             await Windows.System.Launcher.LaunchUriAsync(uri);
         }
-    }
     }
 }
